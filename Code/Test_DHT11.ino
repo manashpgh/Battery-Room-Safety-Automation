358f2023794e80e9a918c44a70fdb2e5ebd72693
Manash Pratim Ghosh <manashpghosh2000@gmail.com>
2026-01-05 18:06:26 +0530

    Update Test_DHT11.ino

--- Code/Test_DHT11.ino
+++ Code/Test_DHT11.ino
@@ -1,1 +1,34 @@
+/*
+ * DHT11 Sensor Test
+ */
 
+#include "DHT.h"
+
+#define DHTPIN 2
+#define DHTTYPE DHT11
+
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  Serial.begin(9600);
+  dht.begin();
+  Serial.println("DHT11 Test Started");
+}
+
+void loop() {
+  delay(2000); // DHT11 is slow, wait 2 seconds between reads
+
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+
+  if (isnan(h) || isnan(t)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+
+  Serial.print("Humidity: ");
+  Serial.print(h);
+  Serial.print(" % | Temperature: ");
+  Serial.print(t);
+  Serial.println(" C");
+}
